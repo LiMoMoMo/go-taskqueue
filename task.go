33e8976a7718be840d7eaa4bf6b113b254b3a5fd
limo <inkli@126.com>
2019-06-13 17:06:07 +0800

    update task.go
    update inferface.

--- task.go
+++ task.go
@@ -1,10 +1,44 @@
 package taskqueue
 
+import "fmt"
+
 type Task struct {
-	TaskID int
+	taskID  int
+	msgChan chan MessageInterface
 }
 
 type TaskInterface interface {
-	run()
-	stop()
+	Run()
+	Stop()
+
+	SetChan(chan MessageInterface)
+	SetTaskID(id int)
+	GetTaskID() int
+}
+
+func (task *Task) SetChan(ch chan MessageInterface) {
+	task.msgChan = ch
+}
+
+func (task *Task) SetTaskID(id int) {
+	task.taskID = id
+}
+
+func (task *Task) GetTaskID() int {
+	return task.taskID
+}
+
+func (task *Task) Run() {
+	fmt.Printf("This is Task run function, task id is %d\r\n", task.taskID)
+	task.Finish()
+}
+
+func (task *Task) Finish() {
+	msg := &FinishMessage{Taskid: task.taskID}
+	msg.Action = FINISH
+	task.msgChan <- msg
+}
+
+func (task *Task) Stop() {
+
 }
